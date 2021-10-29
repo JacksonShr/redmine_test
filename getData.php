4a9d91c9912b57dd615e2c8469a92420c4ec498d
jshr <suharev.work.mailbox@yandex.ru>
2021-10-29 08:03:41 +0300

    Added getter of last time updates

---
+++ getData.php
@@ -0,0 +1,21 @@
+<?php
+$path = "http://redmine.softmg.ru/projects/suharev/time_entries.atom?key=ad4e541dd538a5623b88fe3eb9c76d498c540b1d";
+$curDate = date( "Y-m-d", time() );
+
+$xml = simplexml_load_file( $path );
+
+
+foreach( $xml as $entry ){
+
+    $upDate = date( "Y-m-d", strtotime( $entry->updated ) );
+
+    if( $upDate != $curDate ) continue;
+
+//    $taskText = substr( $entry->update, );
+    preg_match('/\((.*?)\)/',  $entry->update, $strArr );
+    var_dump($strArr);
+    echo $entry->title;
+
+    echo "<hr />";
+
+}
