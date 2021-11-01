8b75f987600f3bbef9094af1b698925f26774545
jshr <suharev.work.mailbox@yandex.ru>
2021-11-01 23:12:47 +0300

    Working script (need to make time logging every day)

--- getData.php
+++ getData.php
@@ -1,9 +1,11 @@
 <?php
-$path = "http://redmine.softmg.ru/projects/suharev/time_entries.atom?key=ad4e541dd538a5623b88fe3eb9c76d498c540b1d";
+$atomKey = "ad4e541dd538a5623b88fe3eb9c76d498c540b1d";
+
+
+$path = "http://redmine.softmg.ru/projects/suharev/time_entries.atom?key=" . $atomKey;
 $curDate = date( "Y-m-d", time() );
 
 $xml = simplexml_load_file( $path );
-
 
 foreach( $xml as $entry ){
 
@@ -11,9 +13,19 @@ foreach( $xml as $entry ){
 
     if( $upDate != $curDate ) continue;
 
-    preg_match('/\((.*?)\)/',  $entry->update, $strArr );
-//    var_dump($strArr);
-    echo $entry->title;
+    $itemArr = explode( "(", $entry->title );
+
+    $itemArr[3] = explode(")", $itemArr[2]);
+
+    $itemTitle = $itemArr[1] . $itemArr[3][1];
+    $itemTime = (float) $itemArr[0];
+    $itemStatus = $itemArr[3][0];
+
+    echo $itemTime . "\n";
+
+    $Str =  $itemTitle . " - " . $itemStatus;
+
+    echo $Str;
 
     echo "<hr />";
 
